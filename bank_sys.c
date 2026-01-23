ccc0385025cbd01f5c2a6bc2d4ae6a780550d492
Roger Lopez <rllopez0410@gmail.com>
2026-01-23 05:44:26 -0500

    Refactor account structures and update login flow

--- bank_sys.c
+++ bank_sys.c
@@ -4,21 +4,26 @@
 #include <time.h>
 
 struct Deposit {
-	int deposit_amount;
+	float deposit_amount;
 };
 
 struct Withdraw {
-	int withdraw_amount;
+	float withdraw_amount;
 };
+
+typedef enum {
+	access_granted,
+	access_denied
+} ACCT_VARIFY;
 
 struct Account {
 	char user_name[30];
-	char password[30];
+	char user_password[30];
 	char first_name[30];
 	char last_name[30];
 	char email[30];
-	int checking;
-	int savings;
+	float checking;
+	float savings;
 	struct Deposit deposit_history[11];
 	struct Withdraw withdraw_history[11];
 };
@@ -49,30 +54,27 @@ void create_account_email(char *email, size_t size) {
 }
 
 void create_account(struct Account customer[21], int account_count) {
-	// create a function that creates a user with all there accont info and add it to the array
 	struct Account new_account;
 	create_account_username(new_account.user_name, sizeof(new_account.user_name));
-	create_account_password(new_account.password, sizeof(new_account.password));
+	create_account_password(new_account.user_password, sizeof(new_account.user_password));
 	create_account_fname(new_account.first_name, sizeof(new_account.first_name));
 	create_account_lname(new_account.last_name, sizeof(new_account.last_name));
 	create_account_email(new_account.email, sizeof(new_account.email));
 	new_account.checking = 0;
 	new_account.savings = 0;
 	customer[account_count] = new_account;
+	printf("welcome %s... Your account has been created...\n", new_account.first_name);
 	return;
 }
 
-int main() {
-	struct Account customer[21];
-	int account_count;
-	bool running = true;
-	char menu_options[5][15] = {"Create Account", "Log-In", "Admin Log-In", "Exit"};
-	
-	while (running) {
-		printf("========================\n===Welcome To TR Bank===\n========================\n");
-		for (int i = 0; i < 4; i++) {
-			printf("[%d]==> %s\n", i + 1, menu_options[i]);
-		}
+void log_in_menu(struct Account customer[21], int index) {
+	bool logged_in = true;
+	char customer_options[5][15] = {"View Checking", "View Savings", "Deposit Funds", "Withdraw Funds", "Logout"};
+
+	while (logged_in) {
+		for (int i = 0; i < 5; i++) {
+			printf("[%d]==> %s\n", i + 1, customer_options[i]);
+		}	
 
 		int choice;
 		printf("Choose an option with the corresponding number...\n");
@@ -80,24 +82,82 @@ int main() {
 
 		switch (choice) {
 			case 1:
-				printf("Creating an account...\n");
-				create_account(customer, account_count);
-				account_count++;
+				printf("Viewing checking account...\n");
 				break;
 			case 2:
-				printf("Logging into your account...\n");
+				printf("Viewing Savings account...\n");
 				break;
 			case 3:
-				printf("Loggin into adminstrator account...\n");
+				printf("Depositing funds...\n");
 				break;
 			case 4:
-				printf("Exiting program...\n");
-				running = false;
+				printf("Withdrawing funds...\n");
+				break;
+			case 5:
+				printf("Logging out...\n");
+				logged_in = false;
 				break;
 			default:
-				printf("The given option is not valid...\n");
+				printf("Unknown option. Please try again...\n");
 		}
 	}
-
-	return 0;
+	return;
 }
+
+void customer_log_in(struct Account customer[21], int account_count) {
+
+	int index;
+	char username_typed[30];
+	char password_typed[30];
+	bool username_verified = false;
+	bool password_verified = false;
+	ACCT_VARIFY status;
+
+	printf("Please type in your username...\n");
+	scanf("%29s", &username_typed);
+	for (int i = 0; i < account_count; i++) {
+		if (strcmp(customer[i].user_name, username_typed) == 0) {
+			username_verified = true;
+			index = i;
+			break;
+		} else {
+			index = -1;
+			continue;
+		}	
+	}
+
+	printf("Please type in your password...\n");
+	scanf("%29s", &password_typed);
+	for (int i = 0; i < account_count; i++) {
+		if (strcmp(customer[i].user_password, password_typed) == 0) {
+			password_verified = true;
+			index = i;
+			break;
+		} else {
+			index = -1;
+			continue;
+		}	
+	}
+	
+	if (username_verified && password_verified) {
+		status = access_granted;
+	} else {
+		status = access_denied;
+	}
+	
+	switch (status) {
+		case access_granted:
+			// if access_granted then we want to retrieve the account information
+			log_in_menu(customer, index);
+			break;
+		case access_denied:
+			printf("Username or password were incorrect... Please try again...\n");
+			break;
+		default:
+			printf("Unknown Error");
+	}
+	
+}
+
+int main() {
+	struct Account customer[21];
